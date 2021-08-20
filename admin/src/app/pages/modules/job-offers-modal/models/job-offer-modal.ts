0c6297a0562075b98c7683ad0721eeb31ec90cbc
bringmetheaugust <bringmetheaugust@gmail.com>
2021-08-20 13:35:19 +0300

    remove test files

--- admin/src/app/pages/modules/job-offers-modal/models/job-offer-modal.ts
+++
@@ -1,8 +0,0 @@
-import {PublishStatus} from '@models';
-
-export type JobOfferModalActionType = 'addOrEdit' | 'remove';
-
-export interface JobOfferModalClosePayload {
-  action: JobOfferModalActionType;
-  status: PublishStatus | null;
-}
--- admin/src/app/pages/modules/search-crew/modules/job-offers-history/models/job-offer-history-modal.ts
+++
@@ -1,3 +0,0 @@
-export interface JobOfferHistoryModalDataI {
-  userId: number;
-}
