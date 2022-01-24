6e5202579e2a36e4e18b8406ba96a39bf72382fd
august <bringmetheaugust@gmail.com>
2022-01-24 22:12:06 +0200

    fix sorting by price

--- server/src/services/Product.ts
+++ server/src/services/Product.ts
@@ -227,12 +227,12 @@ export class ProductService {
 		// sorting
 		switch (sort) {
 			case SortType.PRICE_UP: {
-				qb.orderBy('product.price', 'ASC');
+				qb.orderBy('product.price_current', 'ASC');
 				break;
 			}
 
 			case SortType.PRICE_DOWN: {
-				qb.orderBy('product.price', 'DESC');
+				qb.orderBy('product.price_current', 'DESC');
 				break;
 			}
 
