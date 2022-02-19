c0a244cf41b7c600059db6ad381bf5ca2812d20b
Default Name <viktordilmaloik@gmail.com>
2022-02-19 20:49:05 +0200

    change action dashboard card position

--- client/src/app/shared/components/product-preview-card/product-preview-card.module.ts
+++ client/src/app/shared/components/product-preview-card/product-preview-card.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductPreviewCardComponent } from './product-preview-card.component';
-import { cartIcon, cartSelectedIcon, heartIcon, LeprachaunIconRegistryService, LeprachaunIconsModule } from '@shared/modules/leprachaun-icons';
 import { RouterModule } from '@angular/router';
-import { CurtainsDirective } from '@shared/directives/curtains/curtains.directive';
 import { CurtainsModule } from '@shared/directives/curtains/curtains.module';
 import { ProductPriceModule } from '../product-price/product-price.module';
 
