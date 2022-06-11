33ef483d4c5bb7094b4c78e1613cffa99bdad3db
root@netx_vps <root@vpsixfmd.netxi.host>
2022-06-11 18:53:13 -0400

    add Access-Control-Allow-Credentials

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,8 @@ async function runServer() {
 		.use(cookieParser())
 		.use(session(singleConfigServie.getSessionConfig()))
 		.use((req, res, next) => {
-			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Credentials', 'true');
+			res.header('Access-Control-Allow-Origin', 'https://leprechaun.tech');
 			res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 			res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
 			next();
