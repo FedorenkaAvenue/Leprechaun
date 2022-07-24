49a5395dcbd6485e601813eee4a079c754493ad6
Default Name <viktordilmaloik@gmail.com>
2022-07-25 00:08:17 +0300

    add client global environments

---
+++ client/set-env.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+if (process.env.NODE_ENV === 'dev') {
+  require('dotenv').config({ path: '../.env' });
+}
+
+const environment = process.env.NODE_ENV;
+const targetPath = `./src/environments/environment.${environment}.ts`;
+const envConfigFile = `
+export const environment = {
+  langs: "${process.env.LANGS}",
+  appName: "$${process.env.APP_NAME}"
+};
+`;
+
+fs.writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Output generated at ${targetPath}`);
+});
