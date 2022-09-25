ae0db145d12d189398d207c25c2b81fa92cb2588
Default Name <viktordilmaloik@gmail.com>
2022-09-25 23:46:01 +0300

    add static files / translate

---
+++ clients/user/src/app/shared/static/languages.ts
@@ -0,0 +1,12 @@
+import { Languages } from "@shared/models";
+
+export const LANGUAGES: Array<Languages> = [
+    {
+        code: 'ua',
+        label: 'ua'
+    },
+    {
+        code: 'ru',
+        label: 'ru'
+    }
+]
