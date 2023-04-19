84564695b36af704490830c058a69033bf804ebb
Default Name <viktordilmaloik@gmail.com>
2023-04-19 21:33:31 +0300

    change host to domain in relation interceptor

--- clients/user/src/app/shared/interceptors/relative.interceptor.ts
+++ clients/user/src/app/shared/interceptors/relative.interceptor.ts
@@ -1,25 +1,20 @@
 import { isPlatformServer } from "@angular/common";
 import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Inject, Injectable, Injector, PLATFORM_ID } from "@angular/core";
+import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { REQUEST } from "@nguniversal/express-engine/tokens";
+import { environment } from "environments/environment.global";
 
 @Injectable()
 export class RelativePathInterceptor implements HttpInterceptor {
     constructor(
-        private readonly injector: Injector,
         @Inject(PLATFORM_ID) private platformId: string,
-        
         ) {
 
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler) {
       if (request.url.startsWith('assets') && isPlatformServer(this.platformId)) {
-        const req = this.injector.get(REQUEST);
-        const url = req.protocol + '://' + req.get('host') + '/' + request.url;
-        console.log(`host ${req.get('host')}`);
-        
-        console.log(url);
+        const url = environment.domain + '/' + request.url;
         request = request.clone({
           url: url
         });
